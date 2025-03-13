126827274ad7fda43de198da828ed4f94a950e59
Ben Stone <92685454+benstone22@users.noreply.github.com>
2025-03-13 02:36:39 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['benstone22/IronIrohBot', 'benstone22/GameAIFinal'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['benstone22/GameAIFinal'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
