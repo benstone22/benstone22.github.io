8cdd3453e22deac4b8f78516e29a964852edf61e
Ben Stone <92685454+benstone22@users.noreply.github.com>
2025-04-13 23:07:25 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -65,7 +65,7 @@ const CONFIG = {
     instagram: '',
     reddit: '',
     threads: '',
-    hashnode:'benstone22',
+    hashnode: 'benstone22',
     youtube: '', // example: 'pewdiepie'
     udemy: '',
     dribbble: '',
