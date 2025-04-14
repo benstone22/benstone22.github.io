149a53961023688595306290eb9956741e63e4da
Ben Stone <92685454+benstone22@users.noreply.github.com>
2025-04-14 16:59:10 -0400

    Decided on color

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -165,11 +165,11 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'winter',
+    defaultTheme: 'business',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
